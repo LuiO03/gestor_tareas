cb0f567122938b391e3a6545a0324e502cb0a622
Luis Osorio <lui.fenixand.1997@gmail.com>
2025-05-22 00:25:25 -0500

    agregada las librerias

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $url_base = "http://localhost/gestor_tareas/";
+    require_once(__DIR__ . "/view/templates/header.php")
+?>
+
+
+<?php
+    require_once(__DIR__ . "/view/templates/footer.php")
+?>

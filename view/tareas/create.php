bdffe045df82355a89b38b71ac9787175e51f71f
Luis Osorio <lui.fenixand.1997@gmail.com>
2025-05-23 09:54:02 -0500

    eliminadas algunos archivos

---
+++ view/tareas/create.php
@@ -0,0 +1,11 @@
+<?php
+    require_once("../../view/templates/header.php")
+?>
+
+<a href="" class="btn btn-success">
+    Agregar nueva tarea
+</a>
+
+<?php
+    require_once("../../view/templates/footer.php")
+?>
--- view/templates/header.php
+++ view/templates/header.php
@@ -9,6 +9,8 @@
     <title>To do list</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"/>
 
+    <link rel="stylesheet" href="<?php echo $url_base ?>/style/base.css">
+
     <!--=============== REMIXICONS ===============-->
     <link href= "<?php echo $url_base ?>/libs/remix/fonts/remixicon.css" rel="stylesheet">
 
@@ -16,7 +18,7 @@
     <style>
         /* Opcional: submenús en hover para escritorio */
         @media (min-width: 992px) {
-            
+
             .dropdown-menu, .dropdown{
                 transition: all 0.5ms all;
             }
@@ -30,7 +32,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container" style="max-width: 1200px;">
             <!-- Logo alineado -->
-            <a class="navbar-brand d-flex align-items-center" href="#">
+            <a class="navbar-brand d-flex align-items-center" href="<?php echo $url_base ?>/">
                 <img src="<?php echo $url_base ?>/img/logo.png" alt="Logo" style="width: 40px;" class="me-2">
                 <span>to-do-list</span>
             </a>
@@ -46,36 +48,12 @@
                     <!-- Ítem 1 -->
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="menu1" role="button" data-bs-toggle="dropdown">
-                            Gestión
+                            Tareas
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="menu1">
-                            <li><a class="dropdown-item" href="#">Usuarios</a></li>
-                            <li><a class="dropdown-item" href="#">Roles</a></li>
+                            <li><a class="dropdown-item" href="<?php echo $url_base ?>/view/tareas/show.php">Ver Tareas</a></li>
+                            <li><a class="dropdown-item" href="<?php echo $url_base ?>/view/tareas/create.php">Agregar Tarea</a></li>
                             <li><a class="dropdown-item" href="#">Permisos</a></li>
-                        </ul>
-                    </li>
-
-                    <!-- Ítem 2 -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="menu2" role="button" data-bs-toggle="dropdown">
-                            Reportes
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="menu2">
-                            <li><a class="dropdown-item" href="#">Ventas</a></li>
-                            <li><a class="dropdown-item" href="#">Clientes</a></li>
-                            <li><a class="dropdown-item" href="#">Inventario</a></li>
-                        </ul>
-                    </li>
-
-                    <!-- Ítem 3 -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="menu3" role="button" data-bs-toggle="dropdown">
-                            Configuración
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="menu3">
-                            <li><a class="dropdown-item" href="#">Perfil</a></li>
-                            <li><a class="dropdown-item" href="#">Seguridad</a></li>
-                            <li><a class="dropdown-item" href="#">Preferencias</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -83,7 +61,10 @@
         </div>
     </nav>
 
+    <main>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+    
+
+
+    
+
